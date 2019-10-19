fbcc46d750993008922685039bdf897c0fb2be58
Aisha Gregg <aishagregg@gmail.com>
2019-10-19 17:58:40 +0200

    Add and edit modal

--- beer.js
+++ beer.js
@@ -5,3 +5,19 @@ export function createBeer({ beerId, name, description }) {
     description
   };
 }
+
+function toggleBeerDetail(beerId) {
+  const beerDetail = document.querySelector("#beer-detail");
+  beerDetail.classList.toggle("beer-detail-show");
+}
+
+function createToggleModalEvents() {
+  const elements = document.querySelectorAll(
+    "#beer-detail-veil, .close-icon i, .beer"
+  );
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", () => toggleBeerDetail());
+  }
+}
+
+createToggleModalEvents();
