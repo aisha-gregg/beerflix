1460fc7d3f93bc78484ca5c55818ae3a68365636
Aisha Gregg <aishagregg@gmail.com>
2019-10-27 18:03:04 +0100

    sort beers alphabetically

--- filter-beer.js
+++ filter-beer.js
@@ -3,22 +3,22 @@ import { beersRepository } from "./beers-repository.js";
 
 export function createFilterEvents() {
   const input = document.querySelector("#search-beer");
-  input.addEventListener("keypress", event => {
+  input.addEventListener("input", event => {
     let foundBeers = [];
     for (let i = 0; i < beersRepository.beers.length; i++) {
       const beer = beersRepository.beers[i];
-      if (beer.name.includes(event.target.value)) {
+      if (beer.name.toLowerCase().includes(event.target.value.toLowerCase())) {
         foundBeers.push(beer);
       }
     }
+    foundBeers.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+
     renderBeers(foundBeers);
   });
-}
-
-export function filterbeers() {
-  for (let i = 0; i < beersRepository.length; i++) {
-    if (beersRepository.name === input.name) {
-      beersRepository.filter("#search-beer");
-    }
-  }
 }
